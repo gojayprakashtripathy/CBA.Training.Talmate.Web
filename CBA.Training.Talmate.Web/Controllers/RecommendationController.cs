61e84d41caaf42ca6930bacdf0d2db9526fbe907
jayprakash.sftr@yahoo.com <jayprakash.sftr@yahoo.com>
2020-06-03 06:01:22 +0000

    Added recommendation controller and pages

---
+++ CBA.Training.Talmate.Web/Controllers/RecommendationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CBA.Training.Talmate.Web.Controllers
+{
+    public class RecommendationController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Seek()
+        {
+            return await Task.FromResult(View());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> RouteToPM()
+        {
+            return await Task.FromResult(View());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Accept()
+        {
+            return await Task.FromResult(View());
+        }
+    }
+}
