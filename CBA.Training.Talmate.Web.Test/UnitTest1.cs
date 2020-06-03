5e7d0c1fe00dc49be18b702b6ba1443938ebabc4
jayprakash.sftr@yahoo.com <jayprakash.sftr@yahoo.com>
2020-06-03 06:18:02 +0000

    Added unit test project

---
+++ CBA.Training.Talmate.Web.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CBA.Training.Talmate.Web.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
